3053ffd95a03da089120e279cccc5fd75823610e
A.J. Romaniello <ajiellodev@gmail.com>
2021-03-02 12:57:14 -0700

    commit tree d81d65e35607b2d04615f3d6f9813dbbd7e10da6

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :category, presence: true, inclusion: {in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category"}
+  validates :content, length: { minimum: 100 }
+
+  def category?
+    category == "Fiction" || category == "Non-Fiction"
+  end
 end
